0142184e9ef42cfb97af0dd96e8c68a2fb7ea57c
Naveed Raza <naveedraza@gmail.com>
2019-10-22 10:19:12 -0700

    CRUD: Destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   post "/products" => "products#create", as: :products
   get "/products/:id" => "products#show", as: :product
   get "/products" => "products#index"
-  get 'products/new'
-  get 'products/create'
-  
+  delete "/products/:id" => "products#destroy"
+ 
+  root 'welcome#home'
   get('/about', { to: 'welcome#about', as: :about })
   get('/contact_us', { to: 'contacts#index', as: :contact })
   post('/contact_us', { to: 'contacts#create' })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
+  
   get('/home', to: 'welcome#home')
   
   
