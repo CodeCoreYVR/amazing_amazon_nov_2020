fbdfca796cce1fd3fa5e33a79dfa411a91271259
Naveed Raza <naveedraza@gmail.com>
2021-01-12 14:36:49 -0800

    Product New and Create

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   post "/products" => "products#create", as: :products
   get 'products/new'
   get 'products/create'
-  root 'welcome#home'
+  
   get('/about', { to: 'welcome#about', as: :about })
   get('/contact_us', { to: 'contacts#index', as: :contact })
   post('/contact_us', { to: 'contacts#create' })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get('/home', to: 'welcome#home')
-  get('/about', to: 'welcome#about')
-  get('/contact_us', to: 'welcome#contact_us')
+  
+  
   post('/thank_you', to: 'welcome#thank_you')
 
   #bill splitter
