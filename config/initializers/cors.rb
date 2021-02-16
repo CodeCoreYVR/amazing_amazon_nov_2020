852d5f5c3221f3f63eb9bdfe61f0b89d77fc3981
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-02-15 23:39:23 -0800

    Updated serializers and cors route permissions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,12 +2,12 @@ Rails.application.config.middleware.insert_before  0, Rack::Cors  do
 
     allow  do
         # orgin acccepts an array of domain names. These are all the whitelisted DOMAINS that are allowed to 			make CORS request
-        origins  'localhost:5500', '127.0.0.1:5500', 'google.ca' # You can add any domain name of your choice
+        origins  'localhost:5500', '127.0.0.1:5500', 'google.ca', 'localhost:8080', '192.168.1.40:8080' # You can add any domain name of your choice
         resource(
         "/api/*", # only allows CORS requeswts to a path that looks like /api
         headers:  :any, # allow requests to contain any headers
         credentials:  true, # allows us to send cookies through CORS requests
-        methods: [:get, :post, :delete, :patch, :put, :options]
+        methods: [:get, :post, :delete, :patch, :put, :option]
     )
     end
 end
